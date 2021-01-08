cdfe651cdfb30bc0bfe606f9b57f34c69e272971
Ivan Olexyn <ivan@olexyn.com>
2021-01-08 01:00:17 +0100

    add cross

---
+++ model/src/main/java/com/olexyn/abricore/model/Cross.java
@@ -0,0 +1,36 @@
+package com.olexyn.abricore.model;
+
+
+import java.time.Instant;
+import java.util.Map.Entry;
+
+public class Cross {
+
+    Asset risingAsset;
+    Asset fallingAsset;
+
+    /**
+     * Represents two asset quotes crossing on the chart.
+     * @param asset1
+     * @param asset2
+     * @param interval
+     * @param instant
+     */
+    Cross(Asset asset1, Asset asset2, Interval interval, Instant instant) {
+
+        Entry<Instant,AssetSnapshot> asset1Next = asset1.getSnapShots(interval).higherEntry(instant);
+        Entry<Instant,AssetSnapshot> asset1Prev= asset1.getSnapShots(interval).lowerEntry(instant);
+
+        Entry<Instant,AssetSnapshot> asset2Next = asset2.getSnapShots(interval).higherEntry(instant);
+        Entry<Instant,AssetSnapshot> asset2Prev= asset2.getSnapShots(interval).lowerEntry(instant);
+
+        if (asset1Next.getValue().getPrice() > asset2Next.getValue().getPrice()
+        && asset1Prev.getValue().getPrice() < asset2Prev.getValue().getPrice()) {
+            this.risingAsset = asset1;
+            this.fallingAsset = asset2;
+        }
+    }
+}
+
+
+
