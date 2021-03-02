de1447a971e79997a2f27d88b2eb2ec035758f57
Ivan Olexyn <ivan@olexyn.com>
2021-03-02 20:11:48 +0100

    update fileName interval tokens

--- model/src/main/java/com/olexyn/abricore/model/Interval.java
+++ model/src/main/java/com/olexyn/abricore/model/Interval.java
@@ -10,24 +10,23 @@ import java.util.stream.Collectors;
 
 
 public enum Interval {
-    S_1(Duration.ofSeconds(1), "1 second", "567"),
-    S_5(Duration.ofSeconds(5), "5 seconds", "567"),
-    S_15(Duration.ofSeconds(15), "15 seconds", "567"),
-    S_30(Duration.ofSeconds(30), "30 seconds", "30"),
-    M_1(Duration.ofMinutes(1), "1 minute", "60"),
-    M_3(Duration.ofMinutes(3), "3 minutes", "567"),
-    M_5(Duration.ofMinutes(5), "5 minutes", "567"),
-    M_15(Duration.ofMinutes(15), "15 minutes", "567"),
-    M_30(Duration.ofMinutes(30), "30 minutes", "567"),
-    M_45(Duration.ofMinutes(45), "45 minutes", "567"),
-    H_1(Duration.ofHours(1), "1 hour", "567"),
-    H_2(Duration.ofHours(2), "2 hours", "567"),
-    H_3(Duration.ofHours(3), "3 hours", "567"),
-    H_4(Duration.ofHours(4), "4 hours", "567"),
-    D_1(Period.ofDays(1), "1 day", "567"),
-    W_1(Period.ofWeeks(1), "1 week", "567"),
-    MONTH_1(Period.ofMonths(1), "1 month", "567"),
-    Y_1(Period.ofYears(1), "", "567");
+    S_1(Duration.ofSeconds(1), "1 second", "1s"),
+    S_5(Duration.ofSeconds(5), "5 seconds", "5s"),
+    S_15(Duration.ofSeconds(15), "15 seconds", "15s"),
+    S_30(Duration.ofSeconds(30), "30 seconds", "30S"),
+    M_1(Duration.ofMinutes(1), "1 minute", "1"),
+    M_3(Duration.ofMinutes(3), "3 minutes", "3"),
+    M_5(Duration.ofMinutes(5), "5 minutes", "5"),
+    M_15(Duration.ofMinutes(15), "15 minutes", "15"),
+    M_30(Duration.ofMinutes(30), "30 minutes", "30"),
+    M_45(Duration.ofMinutes(45), "45 minutes", "45"),
+    H_1(Duration.ofHours(1), "1 hour", "60"),
+    H_2(Duration.ofHours(2), "2 hours", "120"),
+    H_3(Duration.ofHours(3), "3 hours", "180"),
+    H_4(Duration.ofHours(4), "4 hours", "240"),
+    D_1(Period.ofDays(1), "1 day", "1D"),
+    W_1(Period.ofWeeks(1), "1 week", "1W"),
+    MONTH_1(Period.ofMonths(1), "1 month", "M");
 
     public final TemporalAmount size;
     private final String twLabel;
