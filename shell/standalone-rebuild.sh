ca5d15046bfdfe30d6efb0be7681572b9be4d049
io42630 <ivan@olexyn.com>
2024-03-04 22:46:07 +0100

    reduce max mem

--- shell/standalone-rebuild.sh
+++ shell/standalone-rebuild.sh
@@ -2,4 +2,4 @@
 
 cd .. &&
 mvn clean install -DskipTests &&
-java -Xms8G -Xmx320G -XX:MaxInlineLevel=32 -XX:MaxInlineSize=512 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:24003 -jar ./main/target/main-0.1.jar
+java -Xms8G -Xmx140G -XX:MaxInlineLevel=32 -XX:MaxInlineSize=512 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:24003 -jar ./main/target/main-0.1.jar
--- shell/standalone-run.sh
+++ shell/standalone-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd .. &&
-java -Xms8G -Xmx320G -XX:MaxInlineLevel=32 -XX:MaxInlineSize=512 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar ./target/main-0.1.jar
+java -Xms8G -Xmx140G -XX:MaxInlineLevel=32 -XX:MaxInlineSize=512 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:24003 -jar ./target/main-0.1.jar
